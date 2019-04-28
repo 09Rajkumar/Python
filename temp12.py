0a6711d0dd9f93277e2fc16ef4744ca7c1b13649
unknown <rraut64@gmail.com>
2019-04-28 13:28:07 +0530

    yes done

---
+++ temp12.py
@@ -0,0 +1,8 @@
+#wap to find the no divisible by 3 and 5
+num=59;
+if (num%3 == 0 and num%5 == 0) :
+    print("no is divisible by 3 and 5")
+else:
+    print("not divisible by 3 and 5")
+        
+    
